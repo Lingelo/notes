ffb251fa6a5f1a1b3c402a27268b9a9dd63c944b
Angélo LIMA <angelomiguellima@gmail.com>
2024-12-05 22:11:16 +0100

    Update README.d

--- README.d
+++ README.d
@@ -1,5 +1,10 @@
 ## Install nvm one linux / osx 
 * curl to : `curl -o- htt‌ps://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash`
 * then : `cat install.sh | bash`
+
+## Launch watch tower once
+`docker run -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once`
+
+
   
   
